c68f400530a384de099e9f51bb6d83c065956952
tikobus <95266048+tikobus@users.noreply.github.com>
2023-09-18 22:46:33 +0800

    Update Install.sh

--- src/Install.sh
+++ src/Install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir project
+rm ../composer.json
+composer create-project workerman/webman:1.5.4 project
 cd project
-composer create-project workerman/webman:1.5.4 .
 composer require illuminate/database:^10.23 illuminate/pagination:^10.23 illuminate/events:^10.23 symfony/var-dumper:^6.3 vlucas/phpdotenv:^5.5 workerman/crontab:^1.0 webman/console:^1.2 webman/redis-queue:^1.2 webman/stomp:^1.1 webman/event:^1.0
 cd ..
 rm -f project/README.md
