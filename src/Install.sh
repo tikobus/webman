5b2bbd5e779835367f66623858fc877f531ed459
tikobus <tikobus@qq.com>
2023-09-18 11:58:28 -0400

    Update

--- src/Install.sh
+++ src/Install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 composer create-project workerman/webman:1.5.4 project
+rm -rf vendor
+rm -f composer.*
 rm -f project/README.md
 mv project/* .
 mv project/.gitignore .
 composer require illuminate/database:^10.23 illuminate/pagination:^10.23 illuminate/events:^10.23 symfony/var-dumper:^6.3 vlucas/phpdotenv:^5.5 workerman/crontab:^1.0 webman/console:^1.2 webman/redis-queue:^1.2 webman/stomp:^1.1 webman/event:^1.0
 rm -rf project
 rm -rf src
+
